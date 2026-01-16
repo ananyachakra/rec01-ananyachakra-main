6378660aa963d6933fed57199dc6b5b862470b09
an61ch34 <an61ch34@isdedu.de>
2026-01-16 13:13:06 -0500

    first commit

---
+++ task.py
@@ -0,0 +1,61 @@
+import os
+from requests import get
+import json
+import csv
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+class Task(object):
+    def __init__(self):
+        self.response = get('https://labrinidis.cs.pitt.edu/cs1656/data/hours.json', verify=False)
+        self.hours = json.loads(self.response.content)
+
+    def part4(self):
+        # write output to hours.csv
+        # fields: name, day, time (NO header requested)
+        f = open('hours.csv', 'w', newline='')
+        w = csv.writer(f)
+        for entry in self.hours:
+            w.writerow([entry['name'], entry['day'], entry['time']])
+        f.close()
+
+    def part5(self):
+        # write output to 'part5.txt'
+        fin = open('hours.csv', 'r')
+        contents = fin.read()
+        fin.close()
+
+        f = open('part5.txt', 'w')
+        f.write(contents)
+        f.close()
+
+    def part6(self):
+        # write output to 'part6.txt'
+        fin = open('hours.csv', 'r', newline='')
+        reader = csv.reader(fin)
+
+        f = open('part6.txt', 'w')
+        for row in reader:
+            f.write(str(row))   # no newline, exactly like console list format
+        f.close()
+        fin.close()
+
+    def part7(self):
+        # write output to 'part7.txt'
+        fin = open('hours.csv', 'r', newline='')
+        reader = csv.reader(fin)
+
+        f = open('part7.txt', 'w')
+        for row in reader:
+            for cell in row:
+                f.write(cell)    # no spaces, no newlines
+        f.close()
+        fin.close()
+
+
+if __name__ == '__main__':
+    task = Task()
+    #task.part4()
+    #task.part5()
+    #task.part6()
+    #task.part7()
