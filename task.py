7de00c76864d611fe5a2066a1ed19437fec69bdb
an61ch34 <an61ch34@isdedu.de>
2026-01-16 13:18:28 -0500

    Recitation 1 submission

--- task.py
+++ task.py
@@ -5,18 +5,23 @@ import csv
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
 class Task(object):
     def __init__(self):
-        self.response = get('https://labrinidis.cs.pitt.edu/cs1656/data/hours.json', verify=False)
+        self.response = get(
+            'https://labrinidis.cs.pitt.edu/cs1656/data/hours.json',
+            verify=False
+        )
         self.hours = json.loads(self.response.content)
 
     def part4(self):
         # write output to hours.csv
-        # fields: name, day, time (NO header requested)
-        f = open('hours.csv', 'w', newline='')
+        f = open('hours.csv', 'w')
         w = csv.writer(f)
+
         for entry in self.hours:
             w.writerow([entry['name'], entry['day'], entry['time']])
+
         f.close()
 
     def part5(self):
@@ -31,31 +36,32 @@ class Task(object):
 
     def part6(self):
         # write output to 'part6.txt'
-        fin = open('hours.csv', 'r', newline='')
+        fin = open('hours.csv', 'r')
         reader = csv.reader(fin)
 
         f = open('part6.txt', 'w')
         for row in reader:
-            f.write(str(row))   # no newline, exactly like console list format
+            f.write(str(row))
         f.close()
         fin.close()
 
     def part7(self):
         # write output to 'part7.txt'
-        fin = open('hours.csv', 'r', newline='')
+        fin = open('hours.csv', 'r')
         reader = csv.reader(fin)
 
         f = open('part7.txt', 'w')
         for row in reader:
             for cell in row:
-                f.write(cell)    # no spaces, no newlines
+                f.write(cell)
         f.close()
         fin.close()
 
 
 if __name__ == '__main__':
     task = Task()
-    #task.part4()
-    #task.part5()
-    #task.part6()
-    #task.part7()
+    # Uncomment these when you want to generate the files locally
+    # task.part4()
+    # task.part5()
+    # task.part6()
+    # task.part7()
